27d2c6683212da0d545fd1f379951ed21c9348a4
Vaishnavi Nagrale <Vaishnavinagrale2@gmail.com>
2023-01-20 13:06:19 +0530

    finished project

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.deepPurple,
+          title: const Center(
+            child: Text('😭I am poor😭'),
+          ),
+        ),
+        body: Container(
+          height: 100,
+         width: 100,
+         child: Image(
+            image: AssetImage('assets/Images/Screenshot_20221226_144130_Google.jpg'),),
+        ),
+        ),
+      ),
+   );
+}
+
